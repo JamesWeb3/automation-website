654f95fa2d1d88dee1b6bf633bd6b7eed1f774f8
Sly <james.oldham0604@gmail.com>
2024-08-02 23:41:44 +0200

    updates

--- src/pages/my-trips/index.tsx
+++ src/pages/my-trips/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { MainLayout } from "@/layouts/main-layout";
 import { GridLayout } from "@/layouts/grid-layout";
-import { ItineraryTab } from "@/components/itinerary/itinerary-tab";
-import { ItineraryContent } from "@/components/itinerary/itinerary-content";
+import { ItineraryTab } from "@/components/my-trips/my-trips-tab";
+import { ItineraryContent } from "@/components/my-trips/my-trips-content";
 
 import Navbar from "@/layouts/navbar";
 const TripPage: React.FC = () => {
